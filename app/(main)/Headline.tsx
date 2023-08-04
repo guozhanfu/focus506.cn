defc1f8274c690fa8d3a1db84b4e02e9ea2a90ee
guozhanfu <g245565736@163.com>
2023-08-04 13:10:13 +0800

    update

--- app/(main)/Headline.tsx
+++ app/(main)/Headline.tsx
@@ -80,10 +80,9 @@ export function Headline() {
         }}
       >
         <Balancer>
-          我是 Cali，
-          <PeekabooLink href="https://zolplay.cn">佐玩</PeekabooLink>
-          创始人，目前带领着佐玩致力于创造一个充满创造力的工作环境，同时鼓励团队创造影响世界的产品。
-          我热爱开发，设计，创新，享受生活，以及在未知领域中探索。
+          我是 Focus，
+          <PeekabooLink href="https://www.focus506.cn/FocusHomePage">Focus</PeekabooLink>
+          创始人，热爱开发，设计，创新，享受生活，以及在未知领域中探索。
         </Balancer>
       </motion.p>
       <motion.div
