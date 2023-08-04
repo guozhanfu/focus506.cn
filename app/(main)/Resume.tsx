dda3895743ca1368f5ca9466b03fa5e178611b5a
guozhanfu <g245565736@163.com>
2023-08-04 13:42:56 +0800

    update

--- app/(main)/Resume.tsx
+++ app/(main)/Resume.tsx
@@ -16,28 +16,35 @@ type Resume = {
 }
 const resume: Resume[] = [
   {
-    company: '深圳市枫叶在线教育',
-    title: 'iOS开发工程师',
+    company: '深圳市佐玩信息技术有限公司',
+    title: '创始人 CEO',
     logo: zolplayLogo,
-    start: '2022',
+    start: '2021',
     end: {
       label: '至今',
       dateTime: new Date().getFullYear(),
     },
   },
   {
-    company: '深圳',
-    title: 'iOS开发工程师',
+    company: 'very very spaceship',
+    title: '软件工程师',
     logo: vvsLogo,
-    start: '2022',
-    end: '2021',
+    start: '2018',
+    end: '2020',
   },
   {
-    company: '热网科技',
-    title: 'iOS开发工程师',
-    logo: abletiveLogo,
-    start: '2021',
+    company: '8ninths Inc.',
+    title: 'AR & 全栈工程师',
+    logo: eightNinthsLogo,
+    start: '2017',
     end: '2018',
+  },
+  {
+    company: 'Abletive',
+    title: '创始人',
+    logo: abletiveLogo,
+    start: '2014',
+    end: '2016',
   },
 ]
 
